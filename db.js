f18f21a8986e69a01dacf3069e26d4e892c96749
candaceyw <candaceyw@gmail.com>
2020-05-20 15:53:29 -0400

    database credentials  update

--- db.js
+++ db.js
@@ -1,11 +1,19 @@
 const Pool = require('pg').Pool;
 
+// const pool = new Pool({
+//   user: 'postgres',
+//   password: 'Bokeh2470',
+//   host: 'localhost',
+//   port: 5432,
+//   database: 'perntodo',
+// });
+
 const pool = new Pool({
-  user: 'postgres',
-  password: 'Bokeh2470',
-  host: 'localhost',
+  user: 'aubqetdxlwktks',
+  password: 'de311c77cfcc78dc8a43430cb8d03e90adabc208c78b8850686082ed617a9a11',
+  host: 'ec2-35-171-31-33.compute-1.amazonaws.com',
   port: 5432,
-  database: 'perntodo',
+  database: 'dd0esb4ijk51mv',
 });
 
 module.exports = pool;
