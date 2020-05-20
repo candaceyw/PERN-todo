be31c069acff186487b4688cbfd7bbdc09d6a645
candaceyw <candaceyw@gmail.com>
2020-05-20 15:51:18 -0400

    db update

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-  user: 'ljiauntjmzcrzd',
-  password: '038c08a2487aa69afafe88add625c9d19e52ee51ad25b40166834b8304cf6a0f',
-  host: 'ec2-23-20-129-146.compute-1.amazonaws.com',
+  user: 'postgres',
+  password: 'Bokeh2470',
+  host: 'localhost',
   port: 5432,
-  database: 'd56kns9fiu3fri',
+  database: 'perntodo',
 });
 
 module.exports = pool;
